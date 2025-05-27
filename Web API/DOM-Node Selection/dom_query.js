e42cbed6db8d0c1592f0e290af3ea032d6fe5f31
ethanhwang <1055256192@qq.com>
2025-05-27 21:16:10 +0800

    Add initial HTML structure and JavaScript for DOM manipulation examples

---
+++ Web API/DOM-Node Selection/dom_query.js
@@ -0,0 +1,9 @@
+console.log("---querySelector Example---");
+
+//1.选择id为"myElement"的元素
+const containerDiv = document.querySelector("#container");
+console.log("Container Div:", containerDiv);
+
+//2.选择第一个class为“highlight”的元素
+const firstHighlight = document.querySelector(".highlight");
+console.log("First Highlight Element:", firstHighlight);
